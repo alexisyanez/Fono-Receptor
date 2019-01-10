f52461bf28e693e71c5f5b0ff2ed0712fac48c8a
platoon <adanilo.yanez@gmail.com>
2019-01-10 12:09:54 -0300

    agregando lineas de sim800l y potenciometros digitales

--- Fono_Receptor/Fono_Receptor.ino
+++ Fono_Receptor/Fono_Receptor.ino
@@ -1,21 +1,28 @@
 /*
   Fading
+ 
+ This example shows how to fade an LED using the analogWrite() function.
+ 
+ The circuit:
+ LED attached from digital pin 9 to ground.
+ 
+ Created 1 Nov 2008
+ By David A. Mellis
+ modified 30 Aug 2011
+ By Tom Igoe
+ 
+ http://www.arduino.cc/en/Tutorial/Fading
+ 
+ This example code is in the public domain.
+ 
+ */
+#include <DigiPotX9Cxxx.h>
+#include <SoftwareSerial.h>
 
-  This example shows how to fade an LED using the analogWrite() function.
+SoftwareSerial mySerial(13, 12); //SIM800L Tx & Rx is connected to Arduino #13 & #12
 
-  The circuit:
-   LED attached from digital pin 9 to ground.
-
-  Created 1 Nov 2008
-  By David A. Mellis
-  modified 30 Aug 2011
-  By Tom Igoe
-
-  http://www.arduino.cc/en/Tutorial/Fading
-
-  This example code is in the public domain.
-
-*/
+DigiPot pot(8,9,10);
+DigiPot pot2(2,9,10);
 
 byte DTMFread; 
 const int STQ = 3;        // Attach DTMF Module STQ Pin to Arduino Digital Pin 3
@@ -24,11 +31,13 @@ const int Q3 = 5;        // Attach DTMF Module Q3  Pin to Arduino Digital Pin 5
 const int Q2 = 6;        // Attach DTMF Module Q2  Pin to Arduino Digital Pin 6
 const int Q1 = 7;        // Attach DTMF Module Q1  Pin to Arduino Digital Pin 7
 
+const int Ton = 11; 
+
 
 
 /*=========================================================================================================
-  loop() : Arduino will interpret the DTMF module output and position the Servo accordingly
-  ========================================================================================================== */
+ loop() : Arduino will interpret the DTMF module output and position the Servo accordingly
+ ========================================================================================================== */
 // Setear contraseña
 int D1 = 2;
 int D2 = 2;
@@ -36,11 +45,16 @@ int D3 = 1;
 int D4 = 2;
 
 // Definir pin de Fade
-#define FadePin 9
+//#define FadePin 9
 
 void setup()
 {
   Serial.begin(9600);
+
+  //Begin serial communication with Arduino and SIM800L
+  mySerial.begin(9600);
+
+  Serial.println("Initializing..."); 
 
   pinMode(STQ, INPUT);
   pinMode(Q4, INPUT);
@@ -51,17 +65,27 @@ void setup()
 
 void loop()
 {
+
+while(getResponse()!="RING"){
+   mySerial.println("ATA"); //accept call
+   delay(1000);
+   tone(Ton,450,1000);
+   noTone(Ton);
+}
+  
   int myDtmf;
   //if (Tonos()) {
-    Serial.println("Auntenticando ...");
-    if (Autenticacion()) {
-      FadeIn();
-      Serial.println("Para salir presione 0 ");
-      while (true) {
-        myDtmf = NumInst();
-        if (myDtmf == 10) Serial.println(myDtmf); FadeOut(); break; // Detectar cuando se corta la llamada
-      }
+  Serial.println("Auntenticando ...");
+  if (Autenticacion()) {
+    FadeIn();
+    Serial.println("Se da el pase al locutor, terminar presionar 0 ");
+    while (true) {
+      myDtmf = NumInst();
+      if (myDtmf == 10) Serial.println(myDtmf); 
+      FadeOut(); 
+      break; // Detectar cuando se corta la llamada
     }
+  }
   //}
 
 }
@@ -70,7 +94,8 @@ int NumInst() {
   int detec = 0;
   while (true) {
     if (digitalRead(STQ) == HIGH) {   //When a DTMF tone is detected, STQ will read HIGH for the duration of the tone.
-      DTMFread = 0; detec = 1;
+      DTMFread = 0; 
+      detec = 1;
       if (digitalRead(Q1) == HIGH) {  //If Q1 reads HIGH, then add 1 to the DTMFread variable
         DTMFread = DTMFread + 1;
       }
@@ -87,8 +112,8 @@ int NumInst() {
     if(detec==1){
       while(true){
         if (digitalRead(STQ) == LOW){ 
-        detec=0;
-        break;
+          detec=0;
+          break;
         }
       }
       break;
@@ -103,9 +128,9 @@ int NumInst() {
 
 // Read DTMF codes one by one and print it on Serial
 /*int myDtmf;
-  myDtmf = dtmf.getDTMF();
-  if(myDtmf != -1) Serial.println(myDtmf);
-  delay(80); // to avoid getting repeated output.*/
+ myDtmf = dtmf.getDTMF();
+ if(myDtmf != -1) Serial.println(myDtmf);
+ delay(80); // to avoid getting repeated output.*/
 
 
 boolean Tonos() { //
@@ -114,10 +139,12 @@ boolean Tonos() { //
   int m = 0;
 
   while (true) {
-   // outDTMF = dtmf.getDTMF();
+    // outDTMF = dtmf.getDTMF();
     if (outDTMF == 0) ++rep; // Detectar Ring
-    if (outDTMF == -1) return false; break; // Detectar Silencio
-    if (rep == 5) return true; break;
+    if (outDTMF == -1) return false; 
+    break; // Detectar Silencio
+    if (rep == 5) return true; 
+    break;
     delay(80);
   }
 }
@@ -150,21 +177,44 @@ boolean Autenticacion() {
 
 void FadeIn()
 {
-  // fade in from min to max in increments of 5 points:
-  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
-    // sets the value (range from 0 to 255):
-    analogWrite(FadePin, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    delay(30);
-  }
+  for (int i=0; i<100; i++) {
+    Serial.print("Increasing, i = ");
+    Serial.println(i, DEC);
+    pot.increase(1);
+    pot2.decrease(1);
+    delay(200);
+  }  
+  //  // fade in from min to max in increments of 5 points:
+  //  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+  //    // sets the value (range from 0 to 255):
+  //    analogWrite(FadePin, fadeValue);
+  //    // wait for 30 milliseconds to see the dimming effect
+  //    delay(30);
+  //  }
 }
 void FadeOut()
 {
-  // fade out from max to min in increments of 5 points:
-  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
-    // sets the value (range from 0 to 255):
-    analogWrite(FadePin, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    delay(30);
+    for (int i=0; i<100; i++) {
+    Serial.print("Decreasing, i = ");
+    Serial.println(i, DEC);
+    pot.decrease(1);
+    pot2.increase(1);
+    delay(200);
   }
+//  // fade out from max to min in increments of 5 points:
+//  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+//    // sets the value (range from 0 to 255):
+//    analogWrite(FadePin, fadeValue);
+//    // wait for 30 milliseconds to see the dimming effect
+//    delay(30);
+//  }
 }
+
+String getResponse() {
+    String response = "";
+    while(mySerial.available()!=0){
+        response += mySerial.read();
+    }
+    return response;
+}
+
